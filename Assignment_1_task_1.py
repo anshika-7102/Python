04cbb622fe00dbe7900db0319411260a8baacc04
anshika-7102 <anshikap2025@outlook.com>
2026-01-28 22:10:21 +0530

    initial commit

---
+++ Assignment_1_task_1.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+print(f"Addition : ",num1+num2)
+print(f"Substraction : ",num1-num2)
+print(f"Multiplication : ",num1*num2)
+print(f"Division : ",num1/num2)
+
+
